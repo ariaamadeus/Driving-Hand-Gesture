7887a3dc63843d21523fe1932f5fdedde44062e0
ariaamadeus <amadeus.aria@gmail.com>
2023-08-26 12:45:01 +0700

    training

--- theKeyboard.py
+++ theKeyboard.py
@@ -7,6 +7,7 @@ class Keyboard:
     def gear(self, value):
         if self.lastPressed != value:
             if 0 <= value <= 6:
-                pyautogui.keyUp(self.lastPressed)
+                try:pyautogui.keyUp(self.lastPressed)
+                except:pass
                 pyautogui.keyDown(str(value))
                 self.lastPressed = value
