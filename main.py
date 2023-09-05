3e0c33872a517d8d13a6dd519a0d4e354a1c082f
ariaamadeus <amadeus.aria@gmail.com>
2023-09-05 19:11:25 +0700

    clean up

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
 from detect import HandDetection
+import cv2
 
-fontScale = 3 # ukuran font
-maxHands = 2 # 1 player
-flip = False
-show_image = False
-
+shrink = 25 # 0 -> 100
+ofset = (-10,0) # -50 -> 50
+ofsetbox = (0,20) # -50 -> 50
+maxHands = 2
+flip = True
+show_image = True
 det = HandDetection(maxHands = maxHands, flip = flip, show_image = show_image)
-# keyboard = Keyboard()
-    
+win = True
+lastframeWin = True
+op = ['+','-','*','/']
 while det.cap.isOpened():
-    left, right, image = det.detectHandNumber()
-    if right[0]:
-        pad.steerValue = right[1]
-        pad.gasValue = 1 if right[2] > 0.15 else 0
+    if win:
+        if not lastframeWin:
+            print("reward")
         
+        while True:
+            eq = det.rndEquation(op)
+            anss = det.otherOp(eq) # find the suitable ops
+            if len(anss) > 0:
+                break
+        win = False
+    results, image = det.detectHandNumber(shrink, ofsetbox)
     
-    if left[0]:
-        pad.gear(left[1].item())
-        pad.brakeValue = 1 if left[2] > 0.15 else 0
-    pad.update(mode=0)
+    image, imageShrink = det.drawBoxPlace(image, hands = maxHands, ofset = ofsetbox)
+    answer = ''
+    xpos = 0
+    for key in results.keys():
+        if 1 <= int(results[key][0]) <= 4:
+            text = op[int(results[key][0])-1]
+            
+            answer += text
+            print(xpos, results[key][1])
+            if results[key][1] < xpos: 
+                print("masuk")
+                answer = answer[::-1]
+            xpos = results[key][1]
+            image = det.drawText(image, imageShrink, text, 2, results[key][1], ofset=ofset)
+    if not lastframeWin:
+        image = det.drawEquation(image, imageShrink, eq, ofset=ofset)
+    else:
+        image = det.drawEquation(image, imageShrink, ["LO", "", "AD", "", "I", "NG"], ofset=ofset)
+    
+    print(anss, answer)
+    if answer in anss:
+        win = True
+        lastframeWin = True
+    else:
+        lastframeWin = False
+
+    cv2.namedWindow("Detect", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("Detect", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
+    if show_image:
+        cv2.imshow("Detect", image)
+        if det.waitKey() == 'q': break
